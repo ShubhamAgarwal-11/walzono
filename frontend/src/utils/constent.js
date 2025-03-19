f703f7ccdd51e75192960b7dd9c242ab791f7197
unknown <shubham2002jindal@gmail.com>
2025-03-19 23:09:47 +0530

    add backend deploy url in frontend constent file

--- frontend/src/utils/constent.js
+++ frontend/src/utils/constent.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT = "https://localhost:4000/api/user"
+export const USER_API_END_POINT = "https://walzono-1.onrender.com/api/user"
 
-export const PARTNER_API_END_POINT = "http://localhost:4000/api/partner"
+export const PARTNER_API_END_POINT = "https://walzono-1.onrender.com/api/partner"
